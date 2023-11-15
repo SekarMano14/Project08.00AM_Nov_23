a929cd5228c6f41a93cf3996755d22932a155f50
SekarMano14 <sekarmano46@gmail.com>
2023-11-15 08:45:54 +0530

    Kumar's new code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -5,9 +5,9 @@ import org.testng.annotations.Test;
 public class Sample {
 	@Test
 	private void test() {
-		System.out.println("Test1");
 		System.out.println("Test4");
 		System.out.println("Test5");
+		System.out.println("Test6");
 
 	}
 
